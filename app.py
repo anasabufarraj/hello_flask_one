9d252ef4c3511ae66c0cb7600648a546e905c0d2
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-11-19 18:57:26 +0200

    Project backup

--- app.py
+++ app.py
@@ -2,4 +2,16 @@
 # © Anas Abu Farraj
 """Learning Flask"""
 
-print("hello")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return '<h1>Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
